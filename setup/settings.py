b572b4295b477fb9a42331a585c1f96d4b8ac028
Viswanath <alapativiswanath1@gmail.com>
2022-07-08 01:12:18 +0530

    update commit

--- setup/settings.py
+++ setup/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-go)m^^d0w)0fm7umv@+h)1e2i9clqg4(7r!93#f8jf6f50pj#j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
